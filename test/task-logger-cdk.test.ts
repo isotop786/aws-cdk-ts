de42529a0d10135a46a31a8e65a436f2639059ff
isotop786 <marufislam77@gmail.com>
2023-10-11 12:45:13 +0600

    Initial commit

---
+++ test/task-logger-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TaskLoggerCdk from '../lib/task-logger-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/task-logger-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TaskLoggerCdk.TaskLoggerCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
